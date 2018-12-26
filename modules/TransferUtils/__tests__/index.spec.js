cb1551085870698d18c47458193ac0fefa6801b8
guor <guor@mczh.com.cn>
2018-12-26 16:35:35 +0800

    翻译信息转换--带有固定格式的，id + defaultMessage

--- modules/TransferUtils/__tests__/index.spec.js
+++ modules/TransferUtils/__tests__/index.spec.js
@@ -1,5 +1,6 @@
-import {transferJson} from '../index'
+import {transferJson,transferJsonContainsID} from '../index'
 import Ddata from '../data.json'
+import IDdata from '../idData.json'
 
 
 describe('TransferUtils使用',() => {
@@ -27,6 +28,60 @@ describe('TransferUtils使用',() => {
     it('transferJson 异常转译',(done) =>{  
         const demoObj = [];      
         const result = transferJson(undefined);
+        expect(result).toEqual(demoObj) 
+        done()
+    })
+
+    it('transferJsonContainsID 正常转译',(done)=>{
+        const demoObj = [
+            {"人员":
+                {"id":"人员","defaultMessage":"郭荣"}
+            }, 
+            {"基本信息.年龄": 
+                {"id":"基本信息.年龄","defaultMessage":18}
+            }, 
+            {"基本信息.性别": 
+                {"id":"基本信息.性别","defaultMessage":"女"}
+            }, 
+            {"基本信息.英文名": 
+                {"id":"基本信息.英文名","defaultMessage":"guorong"}
+            }, 
+            {"基本信息.名族": 
+                {"id":"基本信息.名族","defaultMessage":"土家族"}
+            }, 
+            {"工作信息.所在单位": 
+                {"id":"工作信息.所在单位","defaultMessage":"杭州美创"}
+            }, 
+            {"工作信息.公司信息.地址": 
+                {"id":"工作信息.公司信息.地址","defaultMessage":"丰潭路508号"}
+            }, 
+            {"工作信息.公司信息.地址详情.省": 
+                {"id":"工作信息.公司信息.地址详情.省","defaultMessage":"浙江省"}
+            }, 
+            {"工作信息.公司信息.地址详情.市": 
+                {"id":"工作信息.公司信息.地址详情.市","defaultMessage":"杭州市"}
+            }, 
+            {"工作信息.公司信息.地址详情.区": 
+                {"id":"工作信息.公司信息.地址详情.区","defaultMessage":"拱墅区"}
+            }, 
+            {"工作信息.公司信息.地址详情.街道": 
+                {"id":"工作信息.公司信息.地址详情.街道","defaultMessage":"祥符街道"}
+            }, 
+            {"工作信息.公司信息.电话": 
+                {"id":"工作信息.公司信息.电话","defaultMessage":"400"}
+            }, 
+            {"工作信息.公司信息.编码": 
+                {"id":"工作信息.公司信息.编码","defaultMessage":"310000"}
+            }
+        ];      
+        const result = transferJsonContainsID(IDdata);
+        expect(result).toEqual(demoObj) 
+        done()
+    })
+
+    it('transferJsonContainsID 异常转译',(done) =>{  
+        const demoObj = [];      
+        const result = transferJsonContainsID(undefined);
         expect(result).toEqual(demoObj) 
         done()
     })
--- modules/TransferUtils/index.js
+++ modules/TransferUtils/index.js
@@ -1,3 +1,8 @@
+
+/**
+ * json转换，国际化标准格式
+ * @param {} data 
+ */
 export function transferJson(data){
     let temp = [];
     try {        
@@ -27,6 +32,47 @@ function getTransferJson(jsons, temp, name, sign) {
             temp.push(arrObj);            
         }else{
             getTransferJson(jsons[key], temp, k, sign);
+        }
+    }
+    return temp || [];
+}
+
+/**
+ * json转换带ID
+ * @param {} data 
+ */
+export function transferJsonContainsID(data) {    
+    let temp = [];
+    try {        
+        if(!!data){
+            temp = getTransferJsonContainsID(data,temp,'','.')  
+        }else{
+            throw("待转译的json对象异常")
+        }          
+    } catch (error) {
+        console.log(error);
+    }
+    return temp;
+}
+
+function getTransferJsonContainsID(jsons, temp, name, sign) {    
+    for(let key in jsons){
+        let k = "";
+        if(name === "" || name === undefined) {
+            k = key; 
+        }else{
+            k = name + sign + key; 
+        }
+        if(jsons[key] instanceof Object){
+            if(jsons[key].hasOwnProperty("defaultMessage")){
+                const arrObj = {};
+                const kKey = k;
+                const info = {"id":k,"defaultMessage":jsons[key].defaultMessage}
+                arrObj[kKey] = info; 
+                temp.push(arrObj);   
+            }else{
+                getTransferJsonContainsID(jsons[key], temp, k, sign);
+            }                     
         }
     }
     return temp || [];
