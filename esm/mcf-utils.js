85344ea35b51e4b70ec5a6c1495a20e4f2fd31f3
龙鸿轩 <ignacio4@126.com>
2019-01-03 09:59:03 +0800

    data为空判断

--- esm/mcf-utils.js
+++ esm/mcf-utils.js
@@ -230,14 +230,16 @@ function fetchDownload(url, options) {
       'Content-Type': 'multipart/form-data;charset=UTF-8'
     }
   })).then(function (res) {
-    var blob = new Blob([res.data]);
-    var a = document.createElement('a');
-    var url = window.URL.createObjectURL(blob);
-    var filename = res.headers.get('Content-Disposition') || "";
-    a.href = url;
-    a.download = decodeURI(filename.replace("attachment;filename=", ""));
-    a.click();
-    window.URL.revokeObjectURL(url);
+    if (res) {
+      var blob = new Blob([res.data]);
+      var a = document.createElement('a');
+      var url = window.URL.createObjectURL(blob);
+      var filename = res.headers.get('Content-Disposition') || "";
+      a.href = url;
+      a.download = decodeURI(filename.replace("attachment;filename=", ""));
+      a.click();
+      window.URL.revokeObjectURL(url);
+    } else console.error('no data!');
   });
 }
 
