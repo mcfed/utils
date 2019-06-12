478e188380ff479ec4316dc201024f471b95baf4
shibo <shoes@shibodeMacBook-Pro.local>
2019-06-12 11:31:39 +0800

    分页默认start 0 end 9

--- modules/FetchUtils/index.js
+++ modules/FetchUtils/index.js
@@ -61,8 +61,8 @@ export function processGraphqlParams(params = {}) {
   return Object.assign(
     {},
     otherParam,
-    current ? { start: (current - 1) * pageSize || 0 } : {},
-    pageSize ? { end: current * pageSize - 1 || 9 } : {},
+    current ? { start: (current - 1) * pageSize || 0 } : { start: 0 },
+    pageSize ? { end: current * pageSize - 1 || 9 } : { end: 0 },
     order
       ? {
           order: order && order.replace(/end$/, "")
